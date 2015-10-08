ca247af7b4ce6c4b005e452efa6a4babdfdd19fb
Sabrina Tee <sabrinatee@me.com>
2015-10-07 22:42:47 -0700

    moving data to tables on sales.html

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var CookieStand = function(place, minCustPerHour, maxCustPerHour, avgCookiesPerC
     return Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCustPerHour);
   };
 
+//function that calculates daily cookies
   this.dailyCookies = function(id) {
     var dailyCookieTotal = 0;
     var hoursOpen = ['10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM'];
@@ -17,14 +18,23 @@ var CookieStand = function(place, minCustPerHour, maxCustPerHour, avgCookiesPerC
       this.cookiesByHourList.push(parseInt(this.randCustPerHour() * this.avgCookiesPerCust));
 
       dailyCookieTotal += this.cookiesByHourList[i];
+
+      var header = document.getElementById(id);
+      var hoursOpenHeader = document.createElement('th');
+      hoursOpenHeader.appendChild(document.createTextNode(hoursOpen[i]));
+      header.appendChild(hoursOpenHeader);
+      console.log("Header working");
+    }
+
+    for (var i=0; i<hoursOpen.length; i++) {
       var list = document.getElementById(id);
-      var item = document.createElement('li');
-      item.appendChild(document.createTextNode(hoursOpen[i] + ': ' + this.cookiesByHourList[i] + " cookies"));
+      var item = document.createElement('td');
+      item.appendChild(document.createTextNode(this.cookiesByHourList[i] + " cookies"));
       list.appendChild(item);
       console.log(place + " Result Count: " + dailyCookieTotal);
     }
 
-    var dailyNewTotal = document.createElement('li');
+    var dailyNewTotal = document.createElement('td');
     dailyNewTotal.appendChild(document.createTextNode('Total: ' + dailyCookieTotal + " cookies"));
     list.appendChild(dailyNewTotal);
 
