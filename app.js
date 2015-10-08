9220239deac58643cfa38c04cb7ac42c4372ce19
Sabrina Tee <sabrinatee@me.com>
2015-10-08 15:10:32 -0700

    working sales table

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+//header of table start
+var storeColumn = document.getElementById("cookieSales");
+var headerRow = document.createElement('tr');
+var newStoreColumn = document.createElement('th');
+newStoreColumn.appendChild(document.createTextNode("Store/Hours"));
+headerRow.appendChild(newStoreColumn);
+storeColumn.appendChild(headerRow);
+console.log("Store hours header");
+
+var hoursOpen = ['10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM'];
+
+for (var i=0; i<hoursOpen.length; i++) {
+  var hoursOpenHeader = document.createElement('th');
+  hoursOpenHeader.appendChild(document.createTextNode(hoursOpen[i]));
+  headerRow.appendChild(hoursOpenHeader);
+  console.log("Header hours working");
+}
+
+var totalColumn = document.createElement('th');
+totalColumn.appendChild(document.createTextNode("Total"));
+headerRow.appendChild(totalColumn);
+console.log("Total cookies header");
+//header of table end
+
+//object constructor
 var CookieStand = function(place, minCustPerHour, maxCustPerHour, avgCookiesPerCust) {
   this.place = place;
   this.minCustPerHour = minCustPerHour;
@@ -5,6 +30,7 @@ var CookieStand = function(place, minCustPerHour, maxCustPerHour, avgCookiesPerC
   this.avgCookiesPerCust = avgCookiesPerCust;
   this.cookiesByHourList = [];
 
+//customer randomiser
   this.randCustPerHour = function() {
     return Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCustPerHour);
   };
@@ -12,48 +38,40 @@ var CookieStand = function(place, minCustPerHour, maxCustPerHour, avgCookiesPerC
 //function that calculates daily cookies
   this.dailyCookies = function(id) {
     var dailyCookieTotal = 0;
-    var hoursOpen = ['10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM'];
-
-    var storeColumn = document.getElementById(id);
-    var newStoreColumn = document.createElement('th');
-    newStoreColumn.appendChild(document.createTextNode("Store/Hours"));
-    storeColumn.appendChild(newStoreColumn);
-    console.log("Starter header");
 
     for (var i=0; i<hoursOpen.length; i++) {
       this.cookiesByHourList.push(parseInt(this.randCustPerHour() * this.avgCookiesPerCust));
 
       dailyCookieTotal += this.cookiesByHourList[i];
-
-      var header = document.getElementById(id);
-      var hoursOpenHeader = document.createElement('th');
-      hoursOpenHeader.appendChild(document.createTextNode(hoursOpen[i]));
-      header.appendChild(hoursOpenHeader);
-      console.log("Header working");
     }
 
-    var row = document.getElementById(id);
-    var newRow = document.createElement('tr');
-    newRow.appendChild(document.createTextNode(this.place));
-    row.appendChild(newRow);
-    console.log("Row working");
+//store location row start
+    var infoRow = document.createElement('tr');
+    var storeLocation = document.createElement('td');
+    storeLocation.appendChild(document.createTextNode(this.place));
+    infoRow.appendChild(storeLocation);
+    storeColumn.appendChild(infoRow);
+    console.log("Place row working");
+//store location row end
 
+//loop for inserting cookie by hour by place
     for (var i=0; i<hoursOpen.length; i++) {
-      var list = document.getElementById(id);
-      var item = document.createElement('td');
-      item.appendChild(document.createTextNode(this.cookiesByHourList[i] + " cookies"));
-      list.appendChild(item);
+      var cookieByHour = document.createElement('td');
+      cookieByHour.appendChild(document.createTextNode(this.cookiesByHourList[i]));
+      infoRow.appendChild(cookieByHour);
       console.log(place + " Result Count: " + dailyCookieTotal);
     }
 
-
+//total cookies by place
     var dailyNewTotal = document.createElement('td');
-    dailyNewTotal.appendChild(document.createTextNode('Total: ' + dailyCookieTotal + " cookies"));
-    list.appendChild(dailyNewTotal);
+    dailyNewTotal.appendChild(document.createTextNode(dailyCookieTotal));
+    infoRow.appendChild(dailyNewTotal);
 
     console.log(this.place + " " + this.cookiesByHourList);
     console.log("Daily Cookie Total: " + dailyCookieTotal);
   };
+  //call function to make new objects below
+  this.dailyCookies();
 };
 
 var pikePlace = new CookieStand('Pike Place Market', 17, 88, 5.2);
@@ -61,9 +79,3 @@ var seaTac = new CookieStand('SeaTac Airport', 6, 44, 1.2);
 var southcenter = new CookieStand('Southcenter Mall', 11, 38, 1.9);
 var bellevue = new CookieStand('Bellevue Square', 20, 48, 3.3);
 var alki = new CookieStand('Alki', 3, 24, 2.6);
-
-pikePlace.dailyCookies('pike');
-seaTac.dailyCookies('seatac');
-southcenter.dailyCookies('southcenter');
-bellevue.dailyCookies('bellevue');
-alki.dailyCookies('alki');
