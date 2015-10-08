906d40900a573e57d28350a82a1cd566102765dd
Sabrina Tee <sabrinatee@me.com>
2015-10-08 11:53:36 -0700

    updated front-facing page, work in progress on table in sales.html

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ var CookieStand = function(place, minCustPerHour, maxCustPerHour, avgCookiesPerC
     var dailyCookieTotal = 0;
     var hoursOpen = ['10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM'];
 
+    var storeColumn = document.getElementById(id);
+    var newStoreColumn = document.createElement('th');
+    newStoreColumn.appendChild(document.createTextNode("Store/Hours"));
+    storeColumn.appendChild(newStoreColumn);
+    console.log("Starter header");
+
     for (var i=0; i<hoursOpen.length; i++) {
       this.cookiesByHourList.push(parseInt(this.randCustPerHour() * this.avgCookiesPerCust));
 
@@ -26,6 +32,12 @@ var CookieStand = function(place, minCustPerHour, maxCustPerHour, avgCookiesPerC
       console.log("Header working");
     }
 
+    var row = document.getElementById(id);
+    var newRow = document.createElement('tr');
+    newRow.appendChild(document.createTextNode(this.place));
+    row.appendChild(newRow);
+    console.log("Row working");
+
     for (var i=0; i<hoursOpen.length; i++) {
       var list = document.getElementById(id);
       var item = document.createElement('td');
@@ -33,6 +45,7 @@ var CookieStand = function(place, minCustPerHour, maxCustPerHour, avgCookiesPerC
       list.appendChild(item);
       console.log(place + " Result Count: " + dailyCookieTotal);
     }
+
 
     var dailyNewTotal = document.createElement('td');
     dailyNewTotal.appendChild(document.createTextNode('Total: ' + dailyCookieTotal + " cookies"));
